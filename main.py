4d905aaed886690876421c21621c5ff6180b769f
nooralam15 <nooralam14@icloud.com>
2022-04-12 14:49:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame
+import os
 
 Width, Height = 900, 500
 Win = pygame.display.set_mode((Width, Height))
@@ -7,6 +8,8 @@ pygame.display.set_caption("First Game!")
 
 White = (255, 255, 255)
 FPS = 60
+
+Yellow_spaceship_image = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
 
 def draw_window():
         Win.fill(White)
